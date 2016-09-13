9633a51d13569b774519b46fe9e2556a7eb96d05
Mike <mehanig@gmail.com>
2016-09-13 15:05:23 +0300

    Create video_concater_and_compressor.sh

---
+++ video_concater_and_compressor.sh
@@ -0,0 +1,7 @@
+for f in ./*.mpg; do echo "file '$f'" >> cut_list.txt; done
+mkdir Concated_video
+ffmpeg -f concat -i cut_list.txt -c copy Concated_video/Concated_Video.mp4
+for f in Concated_video/*.mp4 ; 
+do ffmpeg -i $f  -codec:a copy -vcodec libx264 -preset slow -crf 30 ${f%.mp4}_compressed.mp4; done
+rm cut_list.txt
+rm Concated_video/Concated_Video.MP4
